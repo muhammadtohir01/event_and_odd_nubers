07ad518900bd9c6ecea742887916e886d1aef306
muhammadtohir01 <nabiyevmuhammadtohir@gmail.com>
2022-10-07 22:50:00 +0500

    0 1 chiqti

--- is_even.py
+++ is_even.py
@@ -1,9 +1,8 @@
 #Create a variable "number" and assign it a integer value.
-
 #Print 1 if the number is even, otherwise print 0.
 
 
-number=5678
+number=5673
 a=number%2
-d=a-1
-print(d+1)
+d=1-a
+print(d)
